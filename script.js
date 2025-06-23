7d9c591a22c2b240ac4c4af89ae8172332653052
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-06-23 10:54:35 +0800

    add more features yay

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const screen = document.querySelector('.screen');
+const tago = document.querySelector('.tago');
+
+
+function back() {
+    if (screen.value == "Error") {
+        clearscreen();
+        screen.value = screen.value.slice(0, -1);
+        tago.value = tago.value.slice(0, -1);
+    } else {
+        screen.value = screen.value.slice(0, -1);
+        tago.value = tago.value.slice(0, -1);
+    }
+}
 
 function display(input) {
-    screen.value += input;
+    if (screen.value == "Error") {
+        clearscreen();
+        screen.value += input;
+    } else {
+        screen.value += input;
+    }
+}
+
+function tagoonly(input) {
+    if (screen.value == "Error") {
+        clearscreen();
+        tago.value += input;
+    } else {
+        tago.value += input;
+    }
 }
 
 function clearscreen() {
     screen.value = "";
+    tago.value = "";
 }
 
 function calculate() {
     try {
-        screen.value = eval(screen.value);
+        tago.value = eval(tago.value);
+        screen.value = eval(tago.value);
     }
     catch (error) {
+        tago.value = "Error";
         screen.value = "Error";
     }
 }
